a6733b1a57198ccca23e3ba07a3308f6aa2ec5a0
Sytsmaa <sytsmaa@yahoo.com>
2015-12-10 19:55:54 -0500

    adfsd

--- WebContent/index.php
+++ WebContent/index.php
@@ -3,6 +3,6 @@
 	$msg1 = $test->getMsg();
 	$msg2 = $test->getMsg("2nd");
 	
-	echo "<p>" . $msg1 . "</p>";
-	echo "<p>" . $msg2 . "</p>";
+	echo "<p> msg1=\"" . $msg1 . "\"</p>";
+	echo "<p> msg2=\"" . $msg2 . "\"</p>";
 ?>
