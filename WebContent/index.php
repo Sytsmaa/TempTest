1e8862f008309c41bef66a6795f8ec84e98a3b63
Sytsmaa <sytsmaa@yahoo.com>
2015-12-10 20:20:47 -0500

    WROWRK

--- WebContent/index.php
+++ WebContent/index.php
@@ -1,6 +1,11 @@
 <?php
 	echo "<p>We up here boys</p>";
-	$test = java("Test");
+	
+	$derp = new Java("java.lang.System");
+	$ver = $derp->getProperty("java.version");
+	echo "<p> version=\"" . $ver . "\"</p>";
+	
+	$test = new Java("Test");
 	$msg1 = $test->getMsg();
 	$msg2 = $test->getMsg("2nd");
 	
